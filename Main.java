75bce9fa32d1b17c5f0d517a3ff39a7b587a8f96
deadboy <andrey.swift@gmail.com>
2015-10-29 18:39:01 +0300

    Changed window size and renamed FXML file

--- Main.java
+++ Main.java
@@ -11,9 +11,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("window.fxml"));
         primaryStage.setTitle("Password4Me");
-        primaryStage.setScene(new Scene(root, 470, 240));
+        primaryStage.setScene(new Scene(root, 550, 240));
         primaryStage.setResizable(false);
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("ic2.png")));
         primaryStage.show();
